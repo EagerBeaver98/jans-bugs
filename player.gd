782d2f90bc20339a301211ae06c69799a674c830
Eagerbeaver98 <jooke98@outlook.com>
2025-12-29 13:48:17 -0700

    Added jump controls and changed fall acceleration

--- player.gd
+++ player.gd
@@ -1,7 +1,7 @@
 class_name Player extends CharacterBody3D
 
 @export var speed := 10
-@export var fall_acceleration := 75
+@export var fall_acceleration := 100
 var orientation = Transform3D()
 
 var target_velocity := Vector3.ZERO
@@ -19,6 +19,8 @@ func _physics_process(delta):
 		input_direction.x -= 1
 	if Input.is_action_pressed("move_right"):
 		input_direction.x += 1
+	if Input.is_action_pressed("jump"):
+		input_direction.y += 3
 	
 	if input_direction != Vector3.ZERO:
 		input_direction = input_direction.normalized()
@@ -28,6 +30,7 @@ func _physics_process(delta):
 	#Ground velocity
 	target_velocity.x = input_direction.x * speed
 	target_velocity.z = input_direction.z * speed
+	target_velocity.y = input_direction.y * speed
 
 	#Vertical velocity
 	if not is_on_floor():
