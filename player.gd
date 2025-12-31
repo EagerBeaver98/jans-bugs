e6c6da7b13efab30663bb9b583449859a056e1da
Eagerbeaver98 <jooke98@outlook.com>
2025-12-30 19:40:48 -0700

    Camera now controls player movement orientation

--- player.gd
+++ player.gd
@@ -19,18 +19,36 @@ func _physics_process(delta):
 		input_direction.x -= 1
 	if Input.is_action_pressed("move_right"):
 		input_direction.x += 1
+	var jump_impulse := 0.0
 	if Input.is_action_pressed("jump"):
-		input_direction.y += 3
-	
-	if input_direction != Vector3.ZERO:
-		input_direction = input_direction.normalized()
+		jump_impulse = 3.0
 
-		$Pivot.basis = Basis.looking_at(input_direction)
+	# Compute camera-relative movement so "forward" moves away from camera
+	var move_dir := Vector3.ZERO
+	var cam := get_node_or_null("Camera3D")
+	if cam:
+		var cam_forward = (global_position - cam.global_position)
+		cam_forward.y = 0
+		if cam_forward.length() > 0.001:
+			cam_forward = cam_forward.normalized()
+		else:
+			cam_forward = -transform.basis.z
+		var cam_right = cam_forward.cross(Vector3.UP).normalized()
+		move_dir = cam_forward * (-input_direction.z) + cam_right * input_direction.x
+		if move_dir != Vector3.ZERO:
+			move_dir = move_dir.normalized()
+			$Pivot.look_at(global_position + move_dir, Vector3.UP)
+	else:
+		if input_direction != Vector3.ZERO:
+			input_direction = input_direction.normalized()
+			$Pivot.basis = Basis.looking_at(input_direction)
 
-	#Ground velocity
-	target_velocity.x = input_direction.x * speed
-	target_velocity.z = input_direction.z * speed
-	target_velocity.y = input_direction.y * speed
+	# Ground / horizontal velocity
+	target_velocity.x = move_dir.x * speed
+	target_velocity.z = move_dir.z * speed
+
+	# Vertical (jump/fall)
+	target_velocity.y = jump_impulse
 
 	#Vertical velocity
 	if not is_on_floor():
