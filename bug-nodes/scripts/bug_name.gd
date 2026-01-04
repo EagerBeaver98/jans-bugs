860276b21754342ff599fd342df458edba94ef63
Eagerbeaver98 <jooke98@outlook.com>
2026-01-04 15:59:39 -0700

    Added bug template script

---
+++ bug-nodes/scripts/bug_name.gd
@@ -0,0 +1,12 @@
+extends Sprite3D
+
+var bug_name: String = "Unnamed Bug"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
